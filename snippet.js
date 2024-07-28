69a571555d07a248d256d2fce429a0f9dd5ca956
Martin Holy <mholy1983@protonmail.com>
2024-07-28 09:16:05 +0200

    remove unused file

--- snippet.js
+++
@@ -1,6 +0,0 @@
-const regex = /\b[13][a-km-zA-HJ-NP-Z1-9]{25,34}\b|\bbc1[a-zA-HJ-NP-Z0-9]{39,59}\b/g;
-let textContent = document.body.innerText;
-console.log('Document text content for test:', textContent);
-let wallets = textContent.match(regex);
-wallets = wallets ? wallets : [];
-console.log('Tested wallets:', wallets);
